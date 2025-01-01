2c57d774923c3b608327060455567af7dadfc9d1
Alireza Rezapour <alireza.rezapour.me@gmail.com>
2025-01-01 16:45:47 -0500

    Ingore target forlder

--- day-1/target/debug/deps/day_1-62becb5684d85c0a.d
+++
@@ -1,5 +0,0 @@
-/home/nolan/Projects/AdventOfCode/day-1/target/debug/deps/day_1-62becb5684d85c0a: src/main.rs
-
-/home/nolan/Projects/AdventOfCode/day-1/target/debug/deps/day_1-62becb5684d85c0a.d: src/main.rs
-
-src/main.rs:
--- day-1/target/debug/deps/day_1-f61e3c50e93c81e7.d
+++
@@ -1,5 +0,0 @@
-/home/nolan/Projects/AdventOfCode/day-1/target/debug/deps/libday_1-f61e3c50e93c81e7.rmeta: src/main.rs
-
-/home/nolan/Projects/AdventOfCode/day-1/target/debug/deps/day_1-f61e3c50e93c81e7.d: src/main.rs
-
-src/main.rs:
